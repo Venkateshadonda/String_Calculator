6562bb9aea94102fe36772b7caa62d3040f90011
Venkatesh Adonda <venkateshadonda655@gmail.com>
2023-05-23 21:18:43 +0530

    Second

--- src/main/java/Incubytes/assessment/String_Calculator.java
+++ src/main/java/Incubytes/assessment/String_Calculator.java
@@ -1,7 +1,49 @@
 package Incubytes.assessment;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class String_Calculator {
+    public static int Add(String input){
+        if(!input.isEmpty()) {
+            List<Integer> outputnumber = strArrToIntList(getSplit(input));
+            negatives(outputnumber);
+            return outputnumber.stream()
+                    .reduce(Integer::sum)
+                    .orElseThrow();
+        }
+        return 0;
+    }
+
+    private static void negatives(List<Integer> outputnumber) {
+        StringBuilder stringBuilder = new StringBuilder();
+        outputnumber.stream()
+                .filter(number->number<0)
+                .forEach(number->stringBuilder.append(number).append(" "));
+        if(!stringBuilder.toString().isEmpty()){
+            throw new RuntimeException("Negative Values Not Allowed "+stringBuilder.toString());
+        }
+    }
+
+    private static List<Integer> strArrToIntList(String[] inputArr) {
+        return Arrays.stream(inputArr)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static String[] getSplit(String input) {
+        if(input.startsWith("//")){
+            Matcher matcher=Pattern.compile("//(.)\n(.*)").matcher(input);
+            if(matcher.matches()){
+                String delimiter = matcher.group(1);
+                String toSplit = matcher.group(2);
+                return toSplit.split(delimiter);
+            }
+            throw new RuntimeException("Wrong custom delimiter format");
+        }
+        return input.split(",|\n");
     }
 }
--- src/test/java/String_Calculator_Test.java
+++ src/test/java/String_Calculator_Test.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class String_Calculator_Test {
+import Incubytes.assessment.String_Calculator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class String_Calculator_Test {
+    @Test
+    void should_return_zero_for_empty_string(){
+        Assertions.assertThat(String_Calculator.Add("")).isEqualTo(0);
+    }
+    @Test
+    void should_return_number_for_one_number(){
+        Assertions.assertThat(String_Calculator.Add("6")).isEqualTo(6);
+    }
+    @Test
+    void should_return_sum_of_two_numbers(){
+        Assertions.assertThat(String_Calculator.Add("6,2")).isEqualTo( 8);
+    }
+    @Test
+    void should_return_sum_of_unknown_amount_of_numbers(){
+        Assertions.assertThat(String_Calculator.Add("1,2,3")).isEqualTo(6);
+    }
+    @Test
+    void should_return_sum_of_numbers_split_by_commas_and_new_lines(){
+        Assertions.assertThat(String_Calculator.Add("1,4\n3")).isEqualTo(8);
+    }
+    @Test
+    void should_return_sum_of_numbers_split_by_custom_delimiter(){
+        Assertions.assertThat(String_Calculator.Add("//;\n1;2;3")).isEqualTo(6);
+    }
+    @Test
+    void should_throw_exception_for_comma_nest_to_new_line(){
+        Assertions.assertThatThrownBy(()->String_Calculator.Add("2,\n3"))
+                .isInstanceOf(NumberFormatException.class);
+    }
+    @Test
+    void should_throw_exception_for_negatives(){
+        Assertions.assertThatThrownBy(()->String_Calculator.Add("1,-2,3,-4"))
+                .hasMessageContaining("Negative Values Not Allowed ")
+                .hasMessageContaining("-2 -4");
+    }
+
 }
+
